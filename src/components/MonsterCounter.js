16e8e942a049c963386c4a0948003f084f65ee36
eaglesham <edward.eaglesham@gmail.com>
2018-07-24 15:37:32 -0400

    refactor compontent into stateless functional component

--- src/components/MonsterCounter.js
+++ src/components/MonsterCounter.js
@@ -1,39 +1,32 @@
-import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
 //powder = humanBrain
 //backwoods = attackedCreator
-
-export class MonsterAttemptCount extends Component {
-    percentToDecimal(decimal) {
-        return ((decimal * 100) + '%')
-    }
-    calcGoalProgress(total, goal) {
-        return this.percentToDecimal(total/goal)
-    }
-    
-    render() {
-        return (
-            <div className="monster-attempt-count">
-                <div className="total-frankensteins-made">
-                    <span>{this.props.total}</span>
-                    <span>Frankensteins made</span>
-                </div>
-                <div className="humanBrain-frankensteins">
-                    <span>{this.props.humanBrain}</span>
-                    <span>with human brains</span>
-                </div>
-                <div className="insubordinate-frankensteins">
-                    <span>{this.props.attackedCreator}</span>
-                    <span>insubordinate Frankensteins</span>
-                </div>
-                <div>
-                    <span>
-                        {this.calcGoalProgress(this.props.total, this.props.goal)}
-                    </span>    
-                </div>   
-            </div>
-
-        )
-    }
+const percentToDecimal = (decimal) => {
+    return ((decimal * 100) + '%')
 }
+const calcGoalProgress = (total, goal) => {
+    return percentToDecimal(total/goal)
+}
+
+export const MonsterAttemptCount = ({total, humanBrain, attackedCreator, goal}) => (
+    <div className="monster-attempt-count">
+        <div className="total-frankensteins-made">
+            <span>{total}</span>
+            <span>Frankensteins made</span>
+        </div>
+        <div className="humanBrain-frankensteins">
+            <span>{humanBrain}</span>
+            <span>with human brains</span>
+        </div>
+        <div className="insubordinate-frankensteins">
+            <span>{attackedCreator}</span>
+            <span>insubordinate Frankensteins</span>
+        </div>
+        <div>
+            <span>
+                {calcGoalProgress(total, goal)}
+            </span>    
+        </div>   
+    </div>
+)
