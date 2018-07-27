d7fc67da3d6075288c29aba2b65c77f6d420a7bd
eaglesham <edward.eaglesham@gmail.com>
2018-07-27 16:55:38 -0400

    add basic form with defaults to add form route

--- src/components/AddMonsterForm.js
+++ src/components/AddMonsterForm.js
@@ -1,3 +1,37 @@
-export const AddMonsterForm = () => (
-    <h1>Add A Monster</h1>
-)
+import { PropTypes, Component } from 'react'
+
+export class AddMonsterForm extends Component {
+    render() {
+        const { voltage, date, humanBrain, attackedCreator } = this.props
+        return (
+            <form className="add-monster-form">
+                <label htmlFor="voltage">Voltage</label>
+                <input id="voltage" type="text" required defaultValue={voltage} />
+                
+                <label htmlFor="date">Date</label>
+                <input id="date" type="date" required defaultValue={date} />
+                
+                <div>
+                    <label htmlFor="humanBrain">Human Brain</label>
+                    <input id="humanBrain" type="checkbox" defaultChecked={humanBrain} />
+                </div>
+                <div>                    
+                    <label htmlFor="attackedCreator">Attacked Creator</label>
+                    <input id="attackedCreator" type="checkbox" defaultChecked={attackedCreator}/>
+                </div>
+            </form>
+        )
+    }
+}
+AddMonsterForm.defaultProps = {
+    voltage: "800v",
+    date: "2017-02-12",
+    humanBrain: false,
+    attackedCreator: false
+}
+AddMonsterForm.PropTypes = {
+    resort: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    humanBrain: PropTypes.bool.isRequired,
+    attackedCreator: PropTypes.bool.isRequired
+}
