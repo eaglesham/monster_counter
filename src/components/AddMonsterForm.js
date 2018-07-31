026baec15ccc88dd75e67e8faba95df6b305ed99
eaglesham <edward.eaglesham@gmail.com>
2018-07-31 15:20:57 -0400

    refactor addmonsterform as stateless component

--- src/components/AddMonsterForm.js
+++ src/components/AddMonsterForm.js
@@ -1,41 +1,37 @@
 import { PropTypes, Component } from 'react'
 
-export class AddMonsterForm extends Component {
-    constructor(props) {
-        super(props)
-        this.submit = this.submit.bind(this)
-    }
-    submit(e) {
+export const AddMonsterForm = ({ voltage, date, humanBrain, attackedCreator }) => {
+    let _voltage, _date, _humanBrain, _attackedCreator
+    
+    const submit = (e) => {
         e.preventDefault()
-        console.log('voltage', this.refs.voltage.value)
-        console.log('date', this.refs.date.value)
-        console.log('human brain', this.refs.humanBrain.checked)
-        console.log('attacked creator', this.refs.attackedCreator.checked)
+        console.log('voltage', _voltage.value)
+        console.log('date', _date.value)
+        console.log('human brain', _humanBrain.checked)
+        console.log('attacked creator', _attackedCreator.checked)
     }
 
-    render() {
-        const { voltage, date, humanBrain, attackedCreator } = this.props
-        return (
-            <form onSubmit={this.submit} className="add-monster-form">
-                <label htmlFor="voltage">Voltage</label>
-                <input id="voltage" type="text" required defaultValue={voltage} ref="voltage" />
-                
-                <label htmlFor="date">Date</label>
-                <input id="date" type="date" required defaultValue={date} ref="date"/>
-                
-                <div>
-                    <label htmlFor="humanBrain">Human Brain</label>
-                    <input id="humanBrain" type="checkbox" defaultChecked={humanBrain} ref="humanBrain" />
-                </div>
-                <div>                    
-                    <label htmlFor="attackedCreator">Attacked Creator</label>
-                    <input id="attackedCreator" type="checkbox" defaultChecked={attackedCreator} ref="attackedCreator" />
-                </div>
-                <button>Add Monster</button>
-            </form>
-        )
-    }
+    return (
+        <form onSubmit={submit} className="add-monster-form">
+            <label htmlFor="voltage">Voltage</label>
+            <input id="voltage" type="text" required defaultValue={voltage} ref={input => _voltage = input} />
+            
+            <label htmlFor="date">Date</label>
+            <input id="date" type="date" required defaultValue={date} ref={input => _date = input}/>
+            
+            <div>
+                <label htmlFor="humanBrain">Human Brain</label>
+                <input id="humanBrain" type="checkbox" defaultChecked={humanBrain} ref={input => _humanBrain = input} />
+            </div>
+            <div>                    
+                <label htmlFor="attackedCreator">Attacked Creator</label>
+                <input id="attackedCreator" type="checkbox" defaultChecked={attackedCreator} ref={input => _attackedCreator = input} />
+            </div>
+            <button>Add Monster</button>
+        </form>
+    )
 }
+
 AddMonsterForm.defaultProps = {
     voltage: "800v",
     date: "2017-02-12",
