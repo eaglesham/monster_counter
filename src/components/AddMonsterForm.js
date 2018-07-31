28125e18742ac06d2ee389a693ce05209d0d4bf4
eaglesham <edward.eaglesham@gmail.com>
2018-07-31 14:48:57 -0400

    add refs with submit button to addmonster form

--- src/components/AddMonsterForm.js
+++ src/components/AddMonsterForm.js
@@ -1,24 +1,37 @@
 import { PropTypes, Component } from 'react'
 
 export class AddMonsterForm extends Component {
+    constructor(props) {
+        super(props)
+        this.submit = this.submit.bind(this)
+    }
+    submit(e) {
+        e.preventDefault()
+        console.log('voltage', this.refs.voltage.value)
+        console.log('date', this.refs.date.value)
+        console.log('human brain', this.refs.humanBrain.checked)
+        console.log('attacked creator', this.refs.attackedCreator.checked)
+    }
+
     render() {
         const { voltage, date, humanBrain, attackedCreator } = this.props
         return (
-            <form className="add-monster-form">
+            <form onSubmit={this.submit} className="add-monster-form">
                 <label htmlFor="voltage">Voltage</label>
-                <input id="voltage" type="text" required defaultValue={voltage} />
+                <input id="voltage" type="text" required defaultValue={voltage} ref="voltage" />
                 
                 <label htmlFor="date">Date</label>
-                <input id="date" type="date" required defaultValue={date} />
+                <input id="date" type="date" required defaultValue={date} ref="date"/>
                 
                 <div>
                     <label htmlFor="humanBrain">Human Brain</label>
-                    <input id="humanBrain" type="checkbox" defaultChecked={humanBrain} />
+                    <input id="humanBrain" type="checkbox" defaultChecked={humanBrain} ref="humanBrain" />
                 </div>
                 <div>                    
                     <label htmlFor="attackedCreator">Attacked Creator</label>
-                    <input id="attackedCreator" type="checkbox" defaultChecked={attackedCreator}/>
+                    <input id="attackedCreator" type="checkbox" defaultChecked={attackedCreator} ref="attackedCreator" />
                 </div>
+                <button>Add Monster</button>
             </form>
         )
     }
